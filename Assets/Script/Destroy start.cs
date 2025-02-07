213f58a3164ac6f7dfdec91e853f52efff2f8329
2356701 <nthnfortier@gmail.com>
2025-02-07 14:43:09 -0500

    all done before name

--- Assets/Script/Destroy start.cs
+++ Assets/Script/Destroy start.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Destroystart : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
@@ -16,10 +15,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
-
-    public void Redemarrer() {
-
-
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+    public void Hide ( ){
+        gameObject.SetActive (false);}
+   
 }
--- Assets/Script/DetectionBalance.cs
+++ Assets/Script/DetectionBalance.cs
@@ -6,19 +6,19 @@ public class DetectionBalance : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        // Affiche le nom et le tag de l'objet en collision
+      
         print(collision.gameObject.name + " : " + collision.gameObject.tag);
 
-        // Vérifie si l'objet a le tag "Balance"
+       
         if(collision.gameObject.CompareTag("Balance"))
         {
-            // Cherche tous les objets ayant le tag "Plateforme" dans la scène
+           
             GameObject[] plateformes = GameObject.FindGameObjectsWithTag("Plateforme");
 
-            // Vérifie si des objets "Plateforme" existent
+          
             if (plateformes.Length > 0)
             {
-                // Détruire le premier objet "Plateforme" trouvé
+            
                 Destroy(plateformes[0]);
             }
         }
