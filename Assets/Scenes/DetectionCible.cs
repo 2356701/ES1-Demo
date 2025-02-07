ce05af1507a971d9e142921292c44aa6d22d9e25
2356701 <nthnfortier@gmail.com>
2025-02-07 11:55:23 -0500

    timer

--- Assets/Scenes/DetectionCible.cs
+++ Assets/Scenes/DetectionCible.cs
@@ -1,14 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DetectionCible : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision){
+    [SerializeField] private float delayTime = 4f;
+    private float timeElapsed = 0f;
+    private bool startTimer = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
         print(collision.gameObject.name + " : " + collision.gameObject.tag);
-        if(collision.gameObject.CompareTag("Cible")){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (collision.gameObject.CompareTag("Cible"))
+        {
+            startTimer = true; 
+        }
+    }
+
+    void Update()
+    {
+        if (startTimer)
+        {
+            timeElapsed += Time.deltaTime;
+            Debug.Log(timeElapsed);
+
+            if (timeElapsed > delayTime)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
         }
     }
 }
